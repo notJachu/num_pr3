d839b9e7bdf415dee1b4f5492219b6995f13c6c3
notJachu <jasiustasiu50@interia.pl>
2025-05-23 16:40:16 +0200

    implemented lagrange

---
+++ lagrange.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+def lagrange_interpolation(x, y, N = 10):
+
+    # select N interpolation points
+
+    index = np.linspace(0, len(x) - 1, N).astype(int)
+    print(index)
+    x_points = x[index]
+    y_points = y[index]
+
+    y_res = np.zeros(len(x))
+
+    for i in range(len(x)):
+
+        current = x[i]
+        total = 0
+
+        for n in range(N):
+            y = y_points[n]
+            for m in range(N):
+                if m != n:
+                    y *= (current - x_points[m]) / (x_points[n] - x_points[m])
+
+            total += y
+
+        y_res[i] = total
+
+    return y_res
+
+
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from lagrange import lagrange_interpolation
+import matplotlib.pyplot as plt
 
 def main():
     raw_data = pd.read_csv("profile_wysokosciowe/2018_paths/SpacerniakGdansk.csv")
@@ -12,12 +14,34 @@ def main():
     x = data[:, 0]
     y = data[:, 1]
 
-    # Normalise y to 0-1
-    y = (y - np.min(y)) / (np.max(y) - np.min(y))
+    # Normalise x to 0-1
+    x = (x - np.min(x)) / (np.max(x) - np.min(x))
 
-    print(y)
+    print(x)
+
+    # print(y)
+
+    # Interpolate
+    N = 30
+    interp = lagrange_interpolation(x, y, N)
+    interpolation_points = np.linspace(0, len(x) - 1, N).astype(int)
 
 
+
+    plt.plot(x, y, 'r-', label='Original Data')
+    plt.plot(x, interp, 'b-', label='Interpolated Data')
+    plt.scatter(x[interpolation_points], y[interpolation_points], color='green', label='Interpolation Points')
+
+    # runge efect
+    plt.xlim(0, 1)
+    plt.ylim(np.min(y) - 1, np.max(y) + 1)
+
+    plt.xlabel('X-axis')
+    plt.ylabel('Y-axis')
+    plt.title('Lagrange Interpolation')
+    plt.legend()
+    plt.grid()
+    plt.show()
 
 
 
